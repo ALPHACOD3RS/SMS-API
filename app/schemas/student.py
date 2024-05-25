4d39164811017639a4bd9f1ccf7a4d621d077290
Naol Ketema <naolketema55@gmail.com>
2024-05-25 23:21:40 +0300

    student schema

--- app/schemas/student.py
+++ app/schemas/student.py
@@ -1,14 +1,22 @@
-from pydantic import BaseModel, EmailStr
-from typing import List
-
+from pydantic import BaseModel
+from datetime import date
+from typing import Optional
 
 class StudentBase(BaseModel):
-    id: int
     first_name: str
     last_name: str
     date_of_birth: date
-    address: str
-    class_id: int
-    
-    
-    
+    address: Optional[str] = None
+
+class StudentCreate(StudentBase):
+    user_id: int
+
+class StudentUpdate(StudentBase):
+    pass
+
+class Student(StudentBase):
+    id: int
+    user_id: int
+
+    class Config:
+        orm_mode = True
