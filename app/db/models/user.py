6d820c4353ebf2582f8d4bab9bdb24d21b6c7d51
Naol Ketema <naolketema55@gmail.com>
2024-05-25 23:20:11 +0300

    user modeld added

--- app/db/models/user.py
+++ app/db/models/user.py
@@ -20,7 +20,7 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True, index=True, nullable=False)
     email = Column(String, unique=True, index=True, nullable=False)
-    hashed_password = Column(String, nullable=False)
+    password = Column(String, nullable=False)
     role = Column(Enum(UserRole), nullable=False)
     
     students = relationship("Student", back_populates="user")
